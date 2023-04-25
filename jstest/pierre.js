fd6987ad935014d4ec8c3aca33ee03ad78553dd8
Lino <linolam26@gmail.com>
2023-04-25 17:06:46 +0200

    ok

--- jstest/pierre.js
+++ jstest/pierre.js
@@ -1,8 +1,33 @@
 const computerSelection = getComputerChoice();
+const buttons = document.querySelectorAll("button")
+const result2 = document.querySelector(".result")
+let affichage = "Quenani"
+let playerPoints = 0
+let computerPoints = 0
+// we use the .forEach method to iterate through each button
+buttons.forEach(button => {
+
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', function () {
+        game(button.className)
+
+    })
+})
+
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+function refreshPage() {
+    window.location.reload(true)
+}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
+
 
 function getComputerChoice() {
     allo = getRandomInt(3);
@@ -14,9 +39,8 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
-
 function play(playerSelection, computerSelection) {
-    console.log(computerSelection);
+    console.log("L'ordinateur a choisi " + computerSelection)
     if (playerSelection == "Rock" && computerSelection == "Paper") {
         return "You Lose! Paper beats Rock.";
     } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
@@ -28,37 +52,69 @@ function play(playerSelection, computerSelection) {
     } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
         return "You Win! Scissors beat Paper.";
     } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-        return "You win! Paper beats Rock.";
+        return "You Win! Paper beats Rock.";
     } else if (playerSelection == computerSelection) {
         return "Égalité.";
     }
 }
 
-function game() {
-    let computerPoints = 0;
-    let playerPoints = 0;
-    for (let i = 0; i <= 4; i++) {
-        let result = play(prompt("Tu prends quoi?"), getComputerChoice());
+function game(choix) {
 
-        if (result.slice(0, 8) == "You Lose") {
-            computerPoints += 1;
-            console.log("Défaite")
-        } else if (result.slice(0, 7) == "You Win") {
-            playerPoints += 1;
-            console.log("Victoire")
+    let result = play(choix, getComputerChoice());
+
+    if (result.slice(0, 8) == "You Lose") {
+        computerPoints += 1;
+        console.log("Défaite")
+        if (computerPoints != 5) {
+            result2.textContent = "Le score est de " + computerPoints + " (ordinateur) à " + playerPoints + "(vous) "
         } else {
-            console.log("égalité");
+            result2.textContent = "L'ordinateur a gagné " + computerPoints + " à " + playerPoints
+            disableButtons()
+
         }
-        console.log(computerPoints, playerPoints);
-    }
-    if (computerPoints > playerPoints) {
-        return ("You are a LOSER...You lost " + computerPoints + " " + "to " + playerPoints);
-    } else if (playerPoints > computerPoints) {
-        return ("You are a WINNER...You won " + playerPoints + " " + "to " + computerPoints);
+
+
+
+
+    } else if (result.slice(0, 7) == "You Win") {
+        playerPoints += 1;
+        console.log("Victoire")
+        if (playerPoints != 5) {
+            result2.textContent = "Le score est de " + playerPoints + " (vous) à " + computerPoints + " (ordinateur)"
+        } else {
+            result2.textContent = "vous avez  gagné " + playerPoints + " à " + computerPoints
+            disableButtons()
+
+
+        }
+
+
     } else {
-        return ("Le match se termine par une Égalité.")
+        console.log("égalité");
+
 
     }
+
+
+    console.log(computerPoints + " " + playerPoints)
+    return
+
+
+
+
 }
 
-console.log(game());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
